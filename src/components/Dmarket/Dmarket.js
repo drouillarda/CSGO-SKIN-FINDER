afec3c8fa3c737c8e6b7b6d5cd00be599c807be6
alex.drouillard <alex.drouillard.9@hotmail.com>
2023-11-23 20:34:27 -0500

    Cards adjusted, tablet completed

--- src/components/Dmarket/Dmarket.js
+++ src/components/Dmarket/Dmarket.js
@@ -4,12 +4,16 @@ import { Card } from "../Card/Card";
 export const Dmarket = () => {
     return (
         <article className="market">
-            <h2 className="market__heading">DMarket</h2>
-            <Card />
-            <div className="market__details">
-                <p>Ak-47, Minwear</p>
-                <p>Float</p>
-                <p>Price</p> 
+            <div className="market__card">
+                <h2 className="market__card--heading">Dmarket</h2>
+                <div className="market__card--lower">
+                    <Card />
+                    <div className="market__card--lower--details">
+                        <p>Ak-47, Minwear</p>
+                        <p>Float</p>
+                        <p>Price</p> 
+                    </div>
+                </div>
             </div>
         </article>
     );
--- src/components/Steam/Steam.js
+++ src/components/Steam/Steam.js
@@ -4,13 +4,15 @@ import { Card } from "../Card/Card";
 export const Steam = () => {
     return (
         <article className="steam">
-            <h2 className="steam__heading">Steam Marketplace</h2>
-            <div className="steam__lower">
-                <Card />
-                <div className="steam__lower--details">
-                    <p>Ak-47, Minwear</p>
-                    <p>Float</p>
-                    <p>Price</p> 
+            <div className="steam__card">
+                <h2 className="steam__card--heading">Steam Marketplace</h2>
+                <div className="steam__card--lower">
+                    <Card />
+                    <div className="steam__card--lower--details">
+                        <p>Ak-47, Minwear</p>
+                        <p>Float</p>
+                        <p>Price</p> 
+                    </div>
                 </div>
             </div>
         </article>
